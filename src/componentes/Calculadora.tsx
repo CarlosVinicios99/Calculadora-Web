a9abfe1e4ee550a96ba0845a60cf814844e96070
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2023-05-28 15:59:36 -0300

    definição dos cases para tratar a alteração do display nos cliques dos botões

--- src/componentes/Calculadora.tsx
+++ src/componentes/Calculadora.tsx
@@ -8,9 +8,46 @@ const Calculadora: FunctionComponent = () => {
 
     const [conteudoDisplay, setConteudoDisplay] = useState<string>("")
 
-    const alterarDisplay = (rotulo: string) => {
-        console.log(rotulo)
+    const ultimoBotaoClicadoEhUmNumero = () => {
+        const numero = Number(conteudoDisplay.charAt(conteudoDisplay.length - 1))
+        return !isNaN(numero)
     }
+
+    const obterRotuloDoBotao = (rotulo: string) => {
+        
+        if(!isNaN(Number(rotulo))){
+            alterarDisplay(conteudoDisplay.concat(rotulo))
+        }
+        else {
+            switch(rotulo){
+                case "X": case "÷": case "-": case "+": case ".":
+                    if(ultimoBotaoClicadoEhUmNumero()){
+                        alterarDisplay(conteudoDisplay.concat(rotulo))
+                    }
+                    break
+                
+                case "+/-":
+                    break
+                    
+                case "=":
+                    break
+                
+                case "C":
+                    break
+
+                case "CE":
+                    break
+                
+                case "S":
+                    break
+            }
+        }
+
+    }
+
+    const alterarDisplay = (conteudo: string) => {
+        setConteudoDisplay(conteudo)
+    }   
  
     return (
         <div className="calculadora-container">
@@ -18,34 +55,34 @@ const Calculadora: FunctionComponent = () => {
 
             <div className="botoes-container">
                 <div className="linha-de-botoes">
-                    <Botao rotulo="S" cor={CorBotao.VERDE} pressionar={alterarDisplay}/>
-                    <Botao rotulo="C" cor={CorBotao.VERDE} pressionar={alterarDisplay}/>
-                    <Botao rotulo="CE" cor={CorBotao.VERDE} pressionar={alterarDisplay}/>
-                    <Botao rotulo="÷" cor={CorBotao.VERDE} pressionar={alterarDisplay}/>
+                    <Botao rotulo="S" cor={CorBotao.VERDE} pressionar={obterRotuloDoBotao}/>
+                    <Botao rotulo="C" cor={CorBotao.VERDE} pressionar={obterRotuloDoBotao}/>
+                    <Botao rotulo="CE" cor={CorBotao.VERDE} pressionar={obterRotuloDoBotao}/>
+                    <Botao rotulo="÷" cor={CorBotao.VERDE} pressionar={obterRotuloDoBotao}/>
                 </div>
                 <div className="linha-de-botoes">
-                    <Botao rotulo="7" cor={CorBotao.CINZA} pressionar={alterarDisplay}/>
-                    <Botao rotulo="8" cor={CorBotao.CINZA} pressionar={alterarDisplay}/>
-                    <Botao rotulo="9" cor={CorBotao.CINZA} pressionar={alterarDisplay}/>
-                    <Botao rotulo="X" cor={CorBotao.VERDE} pressionar={alterarDisplay}/>
+                    <Botao rotulo="7" cor={CorBotao.CINZA} pressionar={obterRotuloDoBotao}/>
+                    <Botao rotulo="8" cor={CorBotao.CINZA} pressionar={obterRotuloDoBotao}/>
+                    <Botao rotulo="9" cor={CorBotao.CINZA} pressionar={obterRotuloDoBotao}/>
+                    <Botao rotulo="X" cor={CorBotao.VERDE} pressionar={obterRotuloDoBotao}/>
                 </div>
                 <div className="linha-de-botoes">
-                    <Botao rotulo="4" cor={CorBotao.CINZA} pressionar={alterarDisplay}/>
-                    <Botao rotulo="5" cor={CorBotao.CINZA} pressionar={alterarDisplay}/>
-                    <Botao rotulo="6" cor={CorBotao.CINZA} pressionar={alterarDisplay}/>
-                    <Botao rotulo="-" cor={CorBotao.VERDE} pressionar={alterarDisplay}/>
+                    <Botao rotulo="4" cor={CorBotao.CINZA} pressionar={obterRotuloDoBotao}/>
+                    <Botao rotulo="5" cor={CorBotao.CINZA} pressionar={obterRotuloDoBotao}/>
+                    <Botao rotulo="6" cor={CorBotao.CINZA} pressionar={obterRotuloDoBotao}/>
+                    <Botao rotulo="-" cor={CorBotao.VERDE} pressionar={obterRotuloDoBotao}/>
                 </div>
                 <div className="linha-de-botoes">
-                    <Botao rotulo="1" cor={CorBotao.CINZA} pressionar={alterarDisplay}/>
-                    <Botao rotulo="2" cor={CorBotao.CINZA} pressionar={alterarDisplay}/>
-                    <Botao rotulo="3" cor={CorBotao.CINZA} pressionar={alterarDisplay}/>
-                    <Botao rotulo="+" cor={CorBotao.VERDE} pressionar={alterarDisplay}/>
+                    <Botao rotulo="1" cor={CorBotao.CINZA} pressionar={obterRotuloDoBotao}/>
+                    <Botao rotulo="2" cor={CorBotao.CINZA} pressionar={obterRotuloDoBotao}/>
+                    <Botao rotulo="3" cor={CorBotao.CINZA} pressionar={obterRotuloDoBotao}/>
+                    <Botao rotulo="+" cor={CorBotao.VERDE} pressionar={obterRotuloDoBotao}/>
                 </div>
                 <div className="linha-de-botoes">
-                    <Botao rotulo="0" cor={CorBotao.CINZA} pressionar={alterarDisplay}/>
-                    <Botao rotulo="." cor={CorBotao.CINZA} pressionar={alterarDisplay}/>
-                    <Botao rotulo="+/-" cor={CorBotao.CINZA} pressionar={alterarDisplay}/>
-                    <Botao rotulo="=" cor={CorBotao.VERDE} pressionar={alterarDisplay}/>
+                    <Botao rotulo="0" cor={CorBotao.CINZA} pressionar={obterRotuloDoBotao}/>
+                    <Botao rotulo="." cor={CorBotao.CINZA} pressionar={obterRotuloDoBotao}/>
+                    <Botao rotulo="+/-" cor={CorBotao.CINZA} pressionar={obterRotuloDoBotao}/>
+                    <Botao rotulo="=" cor={CorBotao.VERDE} pressionar={obterRotuloDoBotao}/>
                 </div>
             </div>
         </div>
