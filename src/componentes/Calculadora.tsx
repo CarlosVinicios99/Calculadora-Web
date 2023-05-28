dfa92aa15c2a84af65a016f2771bb172c31f7e83
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2023-05-28 14:12:09 -0300

    estrutura inicial da calculadora definida

---
+++ src/componentes/Calculadora.tsx
@@ -0,0 +1,20 @@
+import { FunctionComponent } from "react";
+
+
+const Calculadora: FunctionComponent = () => {
+
+    return (
+        <div className="calculadora-container">
+            
+            <div className="botoes-container">
+                <div className="linha-de-botoes">
+
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Calculadora
+
